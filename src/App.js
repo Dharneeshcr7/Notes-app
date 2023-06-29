9034fc7aeed126eef11d90ac895e4688a068bcb4
Dharneeshcr7 <76260467+Dharneeshcr7@users.noreply.github.com>
2023-06-29 13:11:23 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ function App() {
       <NoteState>
         <Router>
           <Navbar />
-          <Alert message="This is amazing React course" />
+          
           <div className="container">
             <Switch>
               <Route exact path="/">
