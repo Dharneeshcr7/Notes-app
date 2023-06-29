e76a100a8f9d1f68f11a33ea19f661b012ed8251
Dharneeshcr7 <76260467+Dharneeshcr7@users.noreply.github.com>
2023-06-29 13:08:35 +0530

    Update Login.js

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,54 +1,82 @@
-import React, {useState} from 'react'
-import { useHistory } from 'react-router-dom'
+import React,{useState} from "react";
+import Alert from "./Alert";
+import { useNavigate } from "react-router-dom";
 
 
-const Login = (props) => {
-    const [credentials, setCredentials] = useState({email: "", password: ""}) 
-    let history = useHistory();
 
-    const handleSubmit = async (e) => {
+const Login = () => {
+    const [creds,setcreds]=useState({email:"",password:""});
+
+    const onChange=(e)=>{
+        setcreds({...creds,[e.target.name]:e.target.value});
+   }
+   let navigate=useNavigate();
+
+    const handleSubmit=async(e)=>{
         e.preventDefault();
-        const response = await fetch("http://localhost:5000/api/auth/login", {
-            method: 'POST',
+        const response = await fetch(`http://localhost:5000/api/auth/login`, {
+            method: "POST",
+      
             headers: {
-                'Content-Type': 'application/json'
+              "Content-Type": "application/json",
+              
             },
-            body: JSON.stringify({email: credentials.email, password: credentials.password})
-        });
-        const json = await response.json()
-        console.log(json);
-        if (json.success){
-            // Save the auth token and redirect
-            localStorage.setItem('token', json.authtoken); 
-            history.push("/");
+      
+            body: JSON.stringify({ email:creds.email, password:creds.password}),
+          });
+          const authtoken = await response.json();
+          console.log(authtoken);
 
-        }
-        else{
-            alert("Invalid credentials");
-        }
+          if(authtoken.success===true){
+            localStorage.setItem('token',authtoken.authtoken);
+            navigate('/');
+
+
+          }
+          else{
+            alert("Invalid Credentials");
+          }
+
+
     }
+  return (
+    <form onSubmit={handleSubmit}>
+    <h2>Login in to NoteBook</h2>
+      <div class="mb-3">
+        <label for="email" class="form-label">
+          Email address
+        </label>
+        <input
+          type="email"
+          class="form-control"
+          id="email"
+          name="email"
+          aria-describedby="emailHelp"
+          onChange={onChange}
+          value={creds.email}
+        />
+        
+      </div>
+      <div class="mb-3">
+        <label for="password" class="form-label">
+          Password
+        </label>
+        <input
+          type="password"
+          class="form-control"
+          id="password"
+          name="password"
+          onChange={onChange}
+          value={creds.password}
+        />
+      </div>
 
-    const onChange = (e)=>{
-        setCredentials({...credentials, [e.target.name]: e.target.value})
-    }
+      <button type="submit" class="btn btn-primary">
+        Submit
+      </button>
+    </form>
+  );
+};
 
-    return (
-        <div>
-            <form  onSubmit={handleSubmit}>
-                <div className="mb-3">
-                    <label htmlFor="email" className="form-label">Email address</label>
-                    <input type="email" className="form-control" value={credentials.email} onChange={onChange} id="email" name="email" aria-describedby="emailHelp" />
-                    <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
-                </div>
-                <div className="mb-3">
-                    <label htmlFor="password" className="form-label">Password</label>
-                    <input type="password" className="form-control" value={credentials.password} onChange={onChange} name="password" id="password" />
-                </div>
+export default Login;
 
-                <button type="submit" className="btn btn-primary">Submit</button>
-            </form>
-        </div>
-    )
-}
-
-export default Login
